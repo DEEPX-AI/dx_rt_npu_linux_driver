9ea32ed51fbd91e9417810f7afa7868a3a44413a
JoonhyunPark <100750487+JoonhyunPark@users.noreply.github.com>
2024-05-14 16:55:12 +0900

    v1.0.0

--- modules/install-udev_rule.sh
+++ modules/install-udev_rule.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# If you need to modify the existing rules,
+# you should instead copy the rule file you wish to modify from /lib/udev/rules.d to /etc/udev/rules.d,
+# keeping the original name of the file. Any files in /etc/udev/rules.d will override files with identical names in /lib/udev/rules.d
 DRIVER_NAME="dxrt"
-RULES_FILE="/usr/lib/udev/rules.d/51-deepx-udev.rules"
+RULES_FILE="/etc/udev/rules.d/51-deepx-udev.rules"
 
 echo "$DRIVER_NAME permissions will be changed(0666)"
 
